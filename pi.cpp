f4d1d9a96bd948cfb8ed18bdd2ad93fb5d986744
jamesmsteele <jamessteele90@gmail.com>
2014-04-14 18:05:28 -0400

    Update pi.cpp

--- pi.cpp
+++ pi.cpp
@@ -1,32 +1,75 @@
-#ifndef PI_H_INCLUDED
-#define PI_H_INCLUDED
+#include "pi.h"
 
-#include "constant.h"
-#include "exception.h"
-#include "exp-integer.h"
-#include <iostream>
-#include <vector>
+using namespace std;
 
-class Pi : public Constant {
-private:
-	std::vector<Expression*> coefficient;
-public:
-    Pi();
-    ~Pi();
+Pi::Pi() {
+	MathExInteger* co	=	new MathExInteger(1);
+	this->coefficient.push_back(co);
+}
 
-    void add(Pi* a);
-    void add(Expression* a);
-    void subtract(Pi* s);
-    void subtract(Expression* s);
-    void multiply(Expression* m);
-    void divide(Expression* d);
-    void negative();
+string Pi::getName() {
+    return "Pi";
+}
 
-    std::string getName();
-    std::string toString();
-    double toDecimal();
-    Expression* getCoefficient();
-};
+string Pi::toString() {
+    return this->getCoefficient()->toString() + "pi";
+}
 
+double Pi::toDecimal() {
+    return 3.1415926535 * this->getCoefficient()->toDecimal();
+}
 
-#endif // PI_H_INCLUDED
+Expression* Pi::getCoefficient() {
+    return this->coefficient.back();
+}
+
+void Pi::add(Expression* a) {
+    throw Exceptions("Cannot add constants with expressions");
+}
+
+void Pi::subtract(Expression* s) {
+    throw Exceptions("Cannot subtract expressions from constants");
+}
+
+void Pi::multiply(Expression* m) {
+    throw Exceptions("Cannot multiply constants by expressions");
+}
+
+void Pi::divide(Expression* d) {
+    throw Exceptions("Cannot divide constants by expressions");
+}
+
+void Pi::add(Pi* a) {
+    try {
+        this->getCoefficient()->add(a->getCoefficient());
+    }
+    catch (Exceptions e) {
+        Expression* exp = new Expression(this->getCoefficient());
+        exp->add(a->getCoefficient());
+        this->coefficient.push_back(exp);
+    }
+}
+
+void Pi::subtract(Pi* s) {
+    try {
+        this->getCoefficient()->subtract(s->getCoefficient());
+    }
+    catch (Exceptions e) {
+        Expression* exp = new Expression(this->getCoefficient());
+        exp->subtract(s->getCoefficient());
+        this->coefficient.push_back(exp);
+    }
+}
+
+void Pi::multiply(Pi* m) {
+    throw Exceptions("Cannot multiply constants");
+}
+
+void Pi::divide(Pi* d) {
+    throw Exceptions("Cannot divide constants");
+}
+
+void Pi::negative() {
+    MathExInteger* neg = new MathExInteger(-1);
+    this->getCoefficient()->multiply(neg);
+}
