bd149efc03b201a5a817fd74300dc96c0c544f45
jonesguy14 <jonesguy14@gmail.com>
2014-04-14 16:09:02 -0400

    Update exp-integer.cpp

--- exp-integer.cpp
+++ exp-integer.cpp
@@ -13,35 +13,41 @@ MathExInteger::MathExInteger(int me_int) { //: me_int(me_int) {
 	this->me_int = me_int;
 }
 
-void MathExInteger::add(MathExInteger* addend) {
-    int result = me_int + addend->getInt();
-    me_int = result;
+void MathExInteger::add(Expression* addend) {
+    if (addend->getName().compare("Integer")==0) {
+        cout << "I am adding" << endl;
+        int result = me_int + addend->getInt();
+        me_int = result;
+        cout << me_int << endl;
+        cout << getInt() << endl;
+    }
+    else {throw Exceptions("Integer Class: Adding non-integer");}
 }
 
-void MathExInteger::subtract(MathExInteger* subtrahend){
-    int result = me_int - subtrahend->getInt();
-    me_int = result;
-}
-
-void MathExInteger::multiply(MathExInteger* multiplicand) {
-    int result = me_int * multiplicand->getInt();
-    me_int = result;
-}
-
-void MathExInteger::add(Expression* addend){
-    throw Exceptions("Adding integer for non integer, will provide implementation for fractions once they exist");
-}
-
-void MathExInteger::subtract(Expression* s) {
-    throw Exceptions("Subbing integer for non integer, will provide implementation for fractions once they exist");
-}
-
-void MathExInteger::divide(Expression* dividend) {
-    throw Exceptions("Dividing integer for non integer, will provide implementation for fractions once they exist");
+void MathExInteger::subtract(Expression* subtrahend) {
+    if (subtrahend->getName().compare("Integer")==0) {
+        cout << "I am subbing" << endl;
+        int result = me_int - subtrahend->getInt();
+        me_int = result;
+        cout << me_int << endl;
+        cout << getInt() << endl;
+    }
+    else throw Exceptions("Integer Class: Subtracting non-integer");
 }
 
 void MathExInteger::multiply(Expression* multiplicand) {
-    throw Exceptions("Dividing integer for non integer, will provide implementation for fractions once they exist");
+    if (multiplicand->getName().compare("Integer")==0) {
+        cout << "I am multiplying" << endl;
+        int result = me_int * multiplicand->getInt();
+        me_int = result;
+        cout << me_int << endl;
+        cout << getInt() << endl;
+    }
+    else throw Exceptions("Integer Class: Multiplying non-integer");
+}
+
+void MathExInteger::divide(Expression* dividend) {
+    throw Exceptions("Integer Class: Dividing non-integer");
 }
 
 void MathExInteger::negative() {
